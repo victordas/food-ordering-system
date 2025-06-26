f290d70cf0d3a536627391aa744cd9ca71410d00
davikt <victordas@msn.com>
2025-06-26 12:50:42 +0200

    Add order domain app service

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/davikt/service/domain/exception/OrderDomainException.java
@@ -0,0 +1,13 @@
+package com.davikt.service.domain.exception;
+
+import com.davikt.domain.exception.DomainException;
+
+public class OrderDomainException extends DomainException {
+    public OrderDomainException(String message) {
+        super(message);
+    }
+
+    public OrderDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/davikt/service/domain/valueobject/TrackingId.java
@@ -0,0 +1,11 @@
+package com.davikt.service.domain.valueobject;
+
+import com.davikt.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+    public TrackingId(UUID value) {
+        super(value);
+    }
+}
